b46863faf4eeb487a6f65d5293b622aff81ed6ea
Saileshwar <saileshwar.bogam@zemosolabs.com>
2022-02-14 00:33:06 +0530

    final commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// javascript for index.html
+
+const container = document.querySelector('.blogs');
+
+const renderPosts = async (term) => {
+    let uri = 'http://localhost:3000/posts';
+  
+    const res = await fetch(uri);
+    const posts = await res.json();
+    console.log(posts);
+  
+    let template = '';
+    posts.forEach(post => {
+      template += `
+        <div class="post">
+          <h2>${post.title}</h2>
+          <p><small>${post.likes} likes</small></p>
+          <p>${post.body.slice(0, 200)}...</p>
+          <a href="/details.html?id=${post.id}">Read more</a>
+        </div>
+      `
+    });
+    container.innerHTML = template;
+  
+  }
+ 
+
+
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
